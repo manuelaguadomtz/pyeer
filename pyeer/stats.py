bcf41ad0e313d7f919acf1921092aaeea442c3ed
Lic. Manuel Aguado Martinez <manuelaguadomtz@gmail.com>
2017-11-02 19:02:17 -0400

    Refactoring code

--- pyeer/stats.py
+++ pyeer/stats.py
@@ -8,16 +8,16 @@ __copyright__ = 'Copyright 2016'
 __author__ = u'Bsc. Manuel Aguado Martínez'
 
 
-def calculate_eer_step_by_step(genuine_match_scores, imp_match_scores,
+def calculate_eer_step_by_step(gscores, iscores,
                                match_thr_step, hist=False):
     """Calculates the Equal Error Rate measure increasing in each step
     the matching threshold in a given value.
 
     Keyword Arguments:
-    genuine_match_scores -- The score of each genuine match
-    imp_match_scores -- The score of each impostor match
+    gscores -- The score of each genuine match
+    iscores -- The score of each impostor match
     match_thr_step -- The value in which increase the threshold at each step
-    hist -- Indicates whether the impostor_match_scores came in histogram
+    hist -- Indicates whether the iscores came in histogram
             format. Avalaible only for integer matching thresholds step.
 
     :returns a tuple with the folling format: (thresholds, false match rates,
@@ -28,86 +28,91 @@ def calculate_eer_step_by_step(genuine_match_scores, imp_match_scores,
 
     match_thr = 0
     if hist:
-        maximum_thr = max(max(genuine_match_scores), len(imp_match_scores))
+        maximum_thr = max(max(gscores), len(iscores))
     else:
-        maximum_thr = max(genuine_match_scores)
+        maximum_thr = max(gscores)
 
-    total_true_match = float(len(genuine_match_scores))
+    gscores_number = float(len(gscores))
     if hist:
-        total_false_match = float(sum(imp_match_scores))
-        imp_match_scores = np.cumsum(imp_match_scores)
+        iscores_number = float(sum(iscores))
+        iscores = np.cumsum(iscores)
     else:
-        total_false_match = float(len(imp_match_scores))
+        iscores_number = float(len(iscores))
 
     thresholds = []
-    false_match_rates = []
-    false_non_match_rates = []
+    fm_rates = []
+    fnm_rates = []
 
     while match_thr <= maximum_thr:
         if hist:
-            if match_thr < len(imp_match_scores):
-                false_match = total_false_match - imp_match_scores[match_thr]
+            if match_thr < len(iscores):
+                false_match = iscores_number - iscores[match_thr]
             else:
                 false_match = 0
         else:
             # List convertion for Python3 compatibility
             false_match = len(list(filter(lambda s: s > match_thr,
-                                          imp_match_scores)))
+                                          iscores)))
 
-        false_match_rates.append(false_match / total_false_match)
+        fm_rates.append(false_match / iscores_number)
 
         # List convertion for Python3 compatibility
-        false_non_match = len(list(filter(lambda s: s <= match_thr,
-                                          genuine_match_scores)))
-        false_non_match_rates.append(false_non_match / total_true_match)
+        fnm = len(list(filter(lambda s: s <= match_thr, gscores)))
+        fnm_rates.append(fnm / gscores_number)
 
         thresholds.append(match_thr)
         match_thr += match_thr_step
 
-    false_match_rates = np.array(false_match_rates)
-    false_non_match_rates = np.array(false_non_match_rates)
-    index = np.argmin(abs(false_match_rates - false_non_match_rates))
-    eer = abs(false_non_match_rates[index] + false_match_rates[index]) / 2.0
+    fm_rates = np.array(fm_rates)
+    fnm_rates = np.array(fnm_rates)
+    index = np.argmin(abs(fm_rates - fnm_rates))
+    eer = abs(fnm_rates[index] + fm_rates[index]) / 2.0
 
-    return thresholds, false_match_rates, false_non_match_rates, eer
+    return thresholds, fm_rates, fnm_rates, eer
 
 
-def calculate_eer(genuine_match_scores, impostor_match_scores):
-    """Calculates the Equal Error Rate measure
+def calculate_eer(gscores, iscores):
+    """Calculates FMR, FNMR and EER
 
-    Keyword Arguments:
-    genuine_match_scores -- The score of each genuine match
-    impostor_match_scores -- The score of each impostor match
+    @param gscores: Genuine matching scores
+    @type gscores: list
+    @param iscores: Impostor matching scores
+    @type giscores: list
 
-    :returns a tuple with the folling format: (thresholds, false match rates,
-        false non match rates, EER value)
+    @return: (thresholds, FMR, FNMR, EER)
+    @rtype: tuple
     """
-    total_true_match = len(genuine_match_scores)
-    total_false_match = len(impostor_match_scores)
-    total = total_true_match + total_false_match
+    gscores_number = len(gscores)
+    iscores_number = len(iscores)
+    scores_number = gscores_number + iscores_number
 
-    genuine_match_scores = zip(genuine_match_scores, [1] * total_true_match)
-    impostor_match_scores = zip(impostor_match_scores, [0] * total_false_match)
+    # Labeling genuine scores as 1 and impostor scores as 0
+    gscores = zip(gscores, [1] * gscores_number)
+    iscores = zip(iscores, [0] * iscores_number)
 
     # Python3 compatibility
-    genuine_match_scores = list(genuine_match_scores)
-    impostor_match_scores = list(impostor_match_scores)
+    gscores = list(gscores)
+    iscores = list(iscores)
 
-    scores = np.array(sorted(genuine_match_scores + impostor_match_scores,
-                             key=operator.itemgetter(0)))
-    sum_true = np.cumsum(scores[:, 1])
-    sum_false = total_false_match - (np.arange(0, total) - sum_true)
+    # Stacking scores
+    scores = np.array(sorted(gscores + iscores, key=operator.itemgetter(0)))
 
-    false_match_rates = np.ones(total + 1)
-    false_non_match_rates = np.zeros(total + 1)
+    # Calculating FNM and FM distributions
+    fnm = np.cumsum(scores[:, 1])
+    fm = iscores_number - (np.arange(0, scores_number) - fnm)
 
-    false_non_match_rates[1:] = sum_true / total_true_match
-    false_match_rates[1:] = sum_false / total_false_match
+    # Calculating FMR and FNMR
+    fm_rates = np.ones(scores_number + 1)
+    fnm_rates = np.zeros(scores_number + 1)
+    fnm_rates[1:] = fnm / gscores_number
+    fm_rates[1:] = fm / iscores_number
 
-    thresholds = np.zeros((total + 1))
+    # Obtaining thresholds
+    thresholds = np.zeros((scores_number + 1))
     thresholds[1:] = scores[:, 0]
 
-    index = np.argmin(abs(false_match_rates - false_non_match_rates))
-    eer = abs(false_non_match_rates[index] + false_match_rates[index]) / 2.0
+    # Computing EER
+    index = np.argmin(abs(fm_rates - fnm_rates))
+    eer = abs(fnm_rates[index] + fm_rates[index]) / 2.0
 
-    return thresholds, false_match_rates, false_non_match_rates, eer
+    return thresholds, fm_rates, fnm_rates, eer
